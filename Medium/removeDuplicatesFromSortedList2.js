0333acc12489e08d4d4a5b4f872fc4d5e2737a90
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-09 13:48:02 -0500

    Completed remove duplicates from sorted list 2

---
+++ Medium/removeDuplicatesFromSortedList2.js
@@ -0,0 +1,25 @@
+var deleteDuplicates = function(head) {
+    var object1 = {};
+      var current = head;
+      while(current){
+          if(object1[current.val]){
+              object1[current.val]++;
+          } else{
+              object1[current.val] = 1;
+          }
+          current = current.next;
+      }
+      while(head && object1[head.val] >= 2){
+          head = head.next;
+      }
+      var current2 = head;
+      while(current2 && current2.next){
+          if(object1[current2.next.val] >= 2){
+              current2.next = current2.next.next;
+          }
+          else{
+              current2 = current2.next;
+          }
+      }
+      return head;
+  };
