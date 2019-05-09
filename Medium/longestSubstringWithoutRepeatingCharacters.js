8eb2e062ca8920dd7eb77151d19a0992e8e20514
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-09 12:57:03 -0500

    Completed longest substring without repeating characters

---
+++ Medium/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,40 @@
+var lengthOfLongestSubstring = function(string) {
+  if(string.length === 1){
+    return 1;
+  }
+  if(string.length === 0){
+    return 0;
+  }
+  var max = 0;
+  var object1 = {};
+  var start = 0; 
+  var array1 =  string.split('');
+  var dups = array1.filter(function(x){return array1.indexOf(x) === array1.lastIndexOf(x)});
+  if(dups.length === string.length){
+    return string.length
+  }
+
+  for(var i = 0; i < array1.length; i++){
+    if(object1[array1[i]]){
+      delete(object1[array1[start]]);
+      max = Math.max(max, i - start);
+      i--;
+      start += 1;
+      
+    }
+    else{
+      object1[array1[i]] =1;
+      
+    }
+    
+  }
+
+  var keys = Object.keys(object1);
+  var dups2 = keys.filter(function(x){return keys.indexOf(x) === keys.lastIndexOf(x)});
+  
+  if(dups2.length > max){
+      max = dups2.length;
+  }
+
+  return max;
+}
