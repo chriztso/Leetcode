f27c6ca3d961c1440d3a58e47dba3410b52df890
Christopher  Tso <christophertso@Christophers-iMac.local>
2021-09-25 13:55:41 -0500

    Added 2nd solution

--- Medium/removeNthNodeFromEndOfList.js
+++ Medium/removeNthNodeFromEndOfList.js
@@ -27,3 +27,20 @@ var removeNthFromEnd = function(head, n) {
   currentTwo.next = currentTwo.next.next;
   return head;
 };
+
+
+//2nd solution
+var removeNthFromEnd = function(head, n) {
+  let p1 = head
+  let p2 = head
+  for(let i = 0; i < n; i++){
+      p1 = p1.next
+  }
+  if(!p1){ return head.next}
+  while(p1.next){
+      p1 = p1.next
+      p2 = p2.next
+  }
+  p2.next = p2.next.next
+  return head
+};
