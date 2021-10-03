4b8a8c3ab6bf620b7dcaa567bccfa6c483148ffb
Christopher  Tso <christophertso@Christophers-iMac.local>
2021-10-03 15:31:51 -0500

    Added second solution

--- Easy/binaryTreePaths.js
+++ Easy/binaryTreePaths.js
@@ -1,24 +1,43 @@
-var binaryTreePaths = function(root) {
+var binaryTreePaths = function (root) {
   var paths = [];
-  if(!root){
+  if (!root) {
     return [];
-  }    
-  if(!root.left && !root.right){
+  }
+  if (!root.left && !root.right) {
     return [root.val.toString()]
   }
-  function recurse(start, combo){
-    if(!start.left && !start.right){
-      paths.push(combo);  
+  function recurse(start, combo) {
+    if (!start.left && !start.right) {
+      paths.push(combo);
       return;
     }
-    if(start.left){
-      recurse(start.left,  combo.concat(start.left.val));
+    if (start.left) {
+      recurse(start.left, combo.concat(start.left.val));
     }
-    if(start.right){
+    if (start.right) {
       recurse(start.right, combo.concat(start.right.val));
     }
   }
   recurse(root, [root.val]);
-  paths = paths.map(function(x){return x.join('->')})
+  paths = paths.map(function (x) { return x.join('->') })
   return paths;
+};
+
+//2nd solution
+var binaryTreePaths = function (root) {
+  let paths = []
+  function traverse(node, nodes) {
+    if (!node.left && !node.right) {
+      let path = nodes.join('->')
+      paths.push(path)
+    }
+    if (node.left) {
+      traverse(node.left, [...nodes, node.left.val])
+    }
+    if (node.right) {
+      traverse(node.right, [...nodes, node.right.val])
+    }
+  }
+  traverse(root, [root.val])
+  return paths
 };
