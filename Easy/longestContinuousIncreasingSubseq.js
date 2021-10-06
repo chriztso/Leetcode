869317338017ddb13c4537cfd12feae9d3879424
Christopher  Tso <christophertso@Christophers-iMac.local>
2021-10-06 18:14:30 -0500

    Added 2nd solution

--- Easy/longestContinuousIncreasingSubseq.js
+++ Easy/longestContinuousIncreasingSubseq.js
@@ -1,21 +1,34 @@
-var findLengthOfLCIS = function(array) {
+var findLengthOfLCIS = function (array) {
   var longest = 1;
   var current = 1;
-  if(array.length === 0){
+  if (array.length === 0) {
     return 0;
   }
-  for(var i = 0; i < array.length; i++){
-    if(array[i] < array[i+1]){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < array[i + 1]) {
       current++;
-      if(current > longest){
-      longest = current;
+      if (current > longest) {
+        longest = current;
       }
     }
-    else{
+    else {
       current = 1;
     }
   }
 
   return longest;
-  
-}   
+
+}
+
+//2nd solution
+var findLengthOfLCIS = function (nums) {
+  let start = 0
+  let maxLen = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      start = i
+    }
+    maxLen = Math.max(i - start + 1, maxLen)
+  }
+  return maxLen
+};
